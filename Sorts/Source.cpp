9ad6841ffc7bb2b0c05c45dde7f4ffff6afca200
АХ <polyakovmymail@gmail.com>
2021-05-29 20:35:31 +0500

    Добавьте файлы проекта.

---
+++ Sorts/Source.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+void quickSort(std::vector<int> &vec, int start,int end) {
+	int i = start, j = end, pivot = vec[end / 2];
+	do {
+		while (vec[i] < pivot)++i;
+		while (vec[j] > pivot)--j;
+		if (i <= j) {
+			std::swap(vec[i++], vec[j--]);
+		}
+	} while (i <= j);
+	if (i < end)quickSort(vec, start+i,end-i);
+	if (j > 0)quickSort(vec, 0, j);
+}
+
+void bubbleSort(std::vector<int>& vec) {
+	for (size_t k = vec.size() - 1; k > 0; --k) {
+		for (size_t i = 0; i < k; ++i) {
+			if (vec[i] > vec[i + 1])
+				std::swap(vec[i], vec[i + 1]);
+		}
+	}
+}
+
+void evenOddSort(std::vector<int>& vec) {
+	for (size_t i = 0, j = 0; i < vec.size(); ++i){
+		if (!(vec[i] & 1)){//проверка младшего бита сначала нечетные, если !=1 то сначала четные
+			std::swap(vec[i], vec[j++]);
+		}
+	}
+}
+void vectorShow(const std::vector<int>& vec) {
+	for (auto &num : vec)
+		std::cout << num << ' ';
+	//copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
+}
+int main() {
+	std::vector<int> vec{ 5,1,4,3,2,6 };
+	//bubbleSort(vec);
+	//evenOddSort(vec);
+	quickSort(vec, 0,vec.size()-1);
+	vectorShow(vec);
+
+
+}

27d207613c6bb70baead7b137d7fb016aaa085b8
BuildTools <unconfigured@null.spigotmc.org>
2021-09-05 17:29:05 +0500

    добавил сортировку слиянием через потоки

--- Sorts/Source.cpp
+++ Sorts/Source.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <thread>
 
-void quickSort(std::vector<int> &vec, int start,int end) {
+void quickSort(std::vector<int>& vec, int start, int end) {
 	int i = start, j = end, pivot = vec[end / 2];
 	do {
 		while (vec[i] < pivot)++i;
@@ -10,7 +11,7 @@ void quickSort(std::vector<int> &vec, int start,int end) {
 			std::swap(vec[i++], vec[j--]);
 		}
 	} while (i <= j);
-	if (i < end)quickSort(vec, start+i,end-i);
+	if (i < end)quickSort(vec, start + i, end - i);
 	if (j > 0)quickSort(vec, 0, j);
 }
 
@@ -24,52 +25,83 @@ void bubbleSort(std::vector<int>& vec) {
 }
 
 void evenOddSort(std::vector<int>& vec) {
-	for (size_t i = 0, j = 0; i < vec.size(); ++i){
-		if (!(vec[i] & 1)){//if ! first even, logic AND
+	for (size_t i = 0, j = 0; i < vec.size(); ++i) {
+		if (!(vec[i] & 1)) {//if ! first even, logic AND
 			std::swap(vec[i], vec[j++]);
 		}
 	}
 }
 void vectorShow(const std::vector<int>& vec) {
-	for (auto &num : vec)
+	for (auto& num : vec)
 		std::cout << num << ' ';
 	//copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
 }
 
-void merge(std::vector<int>& vec, int left, int right, int middle) {
-	int j = left, k = middle + 1, count = right - left + 1;
-	std::vector<int> vecTemp;
-	vecTemp.reserve(count);
-	for (int i = 0; i < count; ++i) {
-		if (j <= middle && k <= right) {
-			if (vec[j] < vec[k])
-				vecTemp.push_back(vec[j++]);
-			else vecTemp.push_back(vec[k++]);
+void merge(std::vector<int>& vec, int left, int middle, int right) {
+	int i = left, j = middle + 1, count = right - left + 1;
+	std::vector<int> tempVec;
+	tempVec.reserve(count);
+	for (int k = 0; k < count; ++k) {
+		if (i <= middle && j <= right) {
+			if (vec[i] < vec[j])
+				tempVec.push_back(vec[i++]);
+			else tempVec.push_back(vec[j++]);
 		}
 		else {
-			if (j <= middle)
-				vecTemp.push_back(vec[j++]);
-			else vecTemp.push_back(vec[k++]);
+			if (i <= middle)
+				tempVec.push_back(vec[i++]);
+			else tempVec.push_back(vec[j++]);
 		}
 	}
-	for (int i = left, j = 0; i <= right; ++i, ++j)
-		vec[i] = vecTemp[j];
+	for (int k = left, i = 0; k <= right; ++k, ++i)
+		vec[k] = tempVec[i];
 }
 
-void mergeSort(std::vector<int> &vec, int left, int right) {
+void mergeSort(std::vector<int>& vec, int left, int right) {
 	if (left >= right)return;
 	int middle = left + (right - left) / 2;
 	mergeSort(vec, left, middle);
 	mergeSort(vec, middle + 1, right);
-	merge(vec, left, right, middle);
+	merge(vec, left, middle, right);
+}
+
+void mergeThreads(std::vector <int>& vec, int left, int middle, int right) {
+	int i = left, j = middle + 1, count = right - left + 1;
+	std::vector<int> tempVec;
+	tempVec.reserve(count);
+	for (int k = 0; k < count; ++k) {
+		if (i <= middle && j <= right) {
+			if (vec[i] < vec[j])
+				tempVec.push_back(vec[i++]);
+			else tempVec.push_back(vec[j++]);
+		}
+		else {
+			if (i <= middle)
+				tempVec.push_back(vec[i++]);
+			else tempVec.push_back(vec[j++]);
+		}
+	}
+	for (int k = left, i = 0; k <= right; ++k, ++i)
+		vec[k] = tempVec[i];
+}
+
+void mergeSortThreads(std::vector<int>& vec, int left, int right) {
+	if (left >= right) return;
+	int middle = left + (right - left) / 2;
+	std::thread firstThread(mergeSortThreads, std::ref(vec), left, middle);
+	std::thread secondThread(mergeSortThreads, std::ref(vec), middle + 1, right);
+	firstThread.join();
+	secondThread.join();
+	mergeThreads(vec, left, middle, right);
 }
 
 int main() {
 	std::vector<int> vec{ 5,1,4,3,2,6 };
 	//bubbleSort(vec);
 	//evenOddSort(vec);
-	std::cout << vec.size();
+	//std::cout << vec.size();
 	//mergeSort(vec, 0, vec.size()-1);
+	mergeSortThreads(vec, 0, vec.size() - 1);
 	//quickSort(vec, 0,vec.size()-1);
 	vectorShow(vec);
 
